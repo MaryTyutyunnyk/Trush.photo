29e5e68a538c4fc772541b840e56638aca037120
Mary Tyutyunnik <mary.tyutyunnik@gmail.com>
2019-05-21 15:14:08 +0300

    Add Slick to the Index and Portfolio pages

---
+++ src/js/components/index.js
@@ -0,0 +1,14 @@
+$(function () {
+	//const customDots = $('#homePageSliderDots');
+	$('#homePageSliderList').slick({
+		vertical: true,
+		//slidesToShow: 1,
+		//slidesToScroll: 1,
+		// appendDots: customDots,
+		dotsClass: "homePageSliderDots",
+		dots: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
+});
--- src/js/components/portfolio.js
+++ src/js/components/portfolio.js
@@ -1,86 +1,40 @@
-// portfolioCarousel function for portfolioBlockImage
-
 $(function () {
-	$(document).on('click', "#previous_arrow", function () {
-		let carusel = $(this).parents(".portfolioBlockImage");
-		leftCarusel(carusel);
-		return false;
+	const prevArrow = $('#previous_arrow');
+	const nextArrow = $('#next_arrow');
+	$('#portfolioCarouselListImage').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		nextArrow: nextArrow,
+		prevArrow: prevArrow,
+		variableWidth: true,
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 959,
+				settings: "unslick"
+			},
+		]
 	});
-
-	$(document).on('click', "#next_arrow", function () {
-		let carusel = $(this).parents(".portfolioBlockImage");
-		rightCarusel(carusel);
-		return false;
-	});
-
-	function leftCarusel(carusel) {
-		let blockWidth = $(carusel).find(".portfolioCarouselListItem").outerWidth();
-		const newElement = $(carusel)
-			.find(".portfolioCarouselList .portfolioCarouselListItem")
-			.eq(-1)
-			.clone();
-		console.log('leftCarusel');
-		addListeners(newElement);
-		newElement.prependTo($(carusel).find(".portfolioCarouselList"));
-
-		$(carusel).find(".portfolioCarouselList").css({"left": "-" + blockWidth + "px"});
-		$(carusel).find(".portfolioCarouselList .portfolioCarouselListItem").eq(-1).remove();
-		$(carusel).find(".portfolioCarouselList").animate({left: "0px"}, 300);
-	}
-
-	function rightCarusel(carusel) {
-		console.log('rightCarusel');
-
-		let blockWidth = $(carusel).find(".portfolioCarouselListItem").outerWidth();
-		$(carusel).find(".portfolioCarouselList")
-			.animate({left: "-" + blockWidth + "px"},
-			300,
-			function () {
-				$(carusel).find(".portfolioCarouselList .portfolioCarouselListItem")
-					.eq(0)
-					.clone()
-					.appendTo($(carusel).find(".portfolioCarouselList"));
-				$(carusel).find(".portfolioCarouselList .portfolioCarouselListItem")
-					.eq(0)
-					.remove();
-				$(carusel).find(".portfolioCarouselList")
-					.css({"left": "0px"});
-			});
-		}
 });
 
 
-// portfolioCarousel function for portfolioBlockCatalogue
-
 $(function () {
-	$(document).on('click', "#left_arrow", function () {
-		let carusel = $(this).parents(".portfolioBlockCatalogue");
-		left_carusel(carusel);
-		return false;
+	const prevArrow = $('#left_arrow');
+	const nextArrow = $('#right_arrow');
+	$('#portfolioCarouselListCatalogue').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		nextArrow: nextArrow,
+		prevArrow: prevArrow,
+		variableWidth: true,
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 959,
+				settings: "unslick"
+			},
+		]
 	});
-
-	$(document).on('click', "#right_arrow", function () {
-		let carusel = $(this).parents(".portfolioBlockCatalogue");
-		right_carusel(carusel);
-		return false;
-	});
-
-	function left_carusel(carusel) {
-		let block_width = $(carusel).find(".portfolioCarouselListItem").outerWidth();
-		$(carusel).find(".portfolioCarouselList .portfolioCarouselListItem").eq(-1).clone().prependTo($(carusel).find(".portfolioCarouselList"));
-		$(carusel).find(".portfolioCarouselList").css({"left": "-" + block_width + "px"});
-		$(carusel).find(".portfolioCarouselList .portfolioCarouselListItem").eq(-1).remove();
-		$(carusel).find(".portfolioCarouselList").animate({left: "0px"}, 300);
-	}
-
-	function right_carusel(carusel) {
-		let block_width = $(carusel).find(".portfolioCarouselListItem").outerWidth();
-		$(carusel).find(".portfolioCarouselList").animate({left: "-" + block_width + "px"}, 300, function () {
-			$(carusel).find(".portfolioCarouselList .portfolioCarouselListItem").eq(0).clone().appendTo($(carusel).find(".portfolioCarouselList"));
-			$(carusel).find(".portfolioCarouselList .portfolioCarouselListItem").eq(0).remove();
-			$(carusel).find(".portfolioCarouselList").css({"left": "0px"});
-		});
-	}
 });
 
 // Portfolio function for switching between two blocks - portfolioBlockImage and portfolioBlockCatalogue
