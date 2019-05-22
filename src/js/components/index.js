91110e3341a9976f7d4e3f417ec2c7cac13feabb
Mary Tyutyunnik <mary.tyutyunnik@gmail.com>
2019-05-22 11:29:37 +0300

    Changes in slider in the Index page

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,13 +1,14 @@
 $(function () {
 	//const customDots = $('#homePageSliderDots');
 	$('#homePageSliderList').slick({
-		vertical: true,
-		//slidesToShow: 1,
+		//vertical: true,
+		slidesToShow: 1,
 		//slidesToScroll: 1,
 		// appendDots: customDots,
 		//dotsClass: "homePageSliderDots",
 		dots: true,
 		arrows: false,
+		adaptiveHeight: true,
 		//autoplay: true,
 		//autoplaySpeed: 2000,
 	});
