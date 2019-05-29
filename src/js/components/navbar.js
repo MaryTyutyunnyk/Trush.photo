d75a47c4e738eef08c8e0516b1336776057ad0b7
Mary Tyutyunnik <mary.tyutyunnik@gmail.com>
2019-05-29 19:53:20 +0300

    Added toggle functions to the navbar items

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,12 +1,30 @@
-// Function for portfolio submenu demonstration
-
 $(function () {
+
+	// Function for portfolio submenu demonstration
+
 	$('#portfolioItem').click(function (event) {
 		event.stopPropagation();
 		$('.navbarNavSubMenu').toggleClass('open');
+		$(".navbarNav__arrow").toggleClass('arrowUp');
 	});
 
 	$('html').click(function () {
 		$('.navbarNavSubMenu').removeClass("open");
+		$(".navbarNav__arrow").removeClass('arrowUp');
+	});
+
+
+	// Function for language submenu demonstration
+
+	$('#navbarLanguageDesktopLink').click(function (event) {
+		event.stopPropagation();
+		$('.navbarLanguageDesktopSubMenu').toggleClass('open');
+		$(".navbarLanguageDesktop__arrow").toggleClass('upArrow');
+	});
+
+	$('html').click(function () {
+		$('.navbarLanguageDesktopSubMenu').removeClass("open");
+		$(".navbarLanguageDesktop__arrow").removeClass('upArrow');
 	});
 });
+
