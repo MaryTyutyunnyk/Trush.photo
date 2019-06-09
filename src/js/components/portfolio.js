58e2688378f9d0cb80254049c6ea686bca54d36f
Mary Tyutyunnyk <mary.tyutyunnyk@gmail.com>
2019-06-09 19:38:29 +0300

    Changes in Portfolio modal window

--- src/js/components/portfolio.js
+++ src/js/components/portfolio.js
@@ -11,7 +11,7 @@ $(function () {
 			autoWidth: true,
 			loop: true,
 			dots: false,
-			responsiveClass:true,
+			responsiveClass: true,
 			responsive: {
 				0: {
 					loop: false,
@@ -41,8 +41,8 @@ $(function () {
 
 	// Remove portfolioBlockImage carousel on mobile devices
 
-	$window.resize(function() {
-		if($window.width() < 960){
+	$window.resize(function () {
+		if ($window.width() < 960) {
 			if (carouselListImageInitiated) {
 				owl.trigger('destroy.owl.carousel');
 				carouselListImageInitiated = false;
@@ -66,7 +66,7 @@ $(function () {
 			autoWidth: true,
 			loop: true,
 			dots: false,
-			responsiveClass:true,
+			responsiveClass: true,
 			responsive: {
 				0: {
 					loop: false,
@@ -96,8 +96,8 @@ $(function () {
 
 	// Remove PortfolioBlockCatalogue on mobile devices
 
-	$window.resize(function() {
-		if($window.width() < 960){
+	$window.resize(function () {
+		if ($window.width() < 960) {
 			if (carouselListCatalogueInitiated) {
 				owl2.trigger('destroy.owl.carousel');
 				carouselListCatalogueInitiated = false;
@@ -128,31 +128,34 @@ $(function () {
 
 
 // Function for modalCarousel
-
-let amount = $(".modalCarouselList > li").length; // Define amount of items
-let curr = 0; // Define index of active item
-$(".modalCarouselList li:eq(" + curr + ")").fadeIn(); // Show item with index 0 (first element)
-
 $(function () {
-	$(".modalCarousel__arrow_previous").on('click', function () {
-		$(".modalCarouselList li:eq(" + curr + ")").fadeOut(100); // Hide item with index 0 (first element)
-		// If items index - 1 less than 0, then back to the last item.
-		// In other case subtract to the previous item
-		if (curr - 1 < 0) {
-			curr = amount - 1;
-		} else curr--;
-		$(".modalCarouselList li:eq(" + curr + ")").delay(100).fadeIn(100); // Show item with index 0 (first element)
+	const modalArrowPrevious = $('#modalArrowPrevious');
+	const modalArrowNext = $('#modalArrowNext');
+	const owlModalCarousel = $('#modalCarouselList');
+	owlModalCarousel.owlCarousel({
+		items: 1,
+		loop: true,
+		dots: false,
+		mouseDrag: false,
+		touchDrag: false,
+		pullDrag: false,
+		freeDrag: false,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				touchDrag: true,
+			},
+			960: {
+				touchDrag: false,
+			},
+		}
 	});
-
-	$(".modalCarousel__arrow_next").on('click', function () {
-		$(".modalCarouselList li:eq(" + curr + ")").fadeOut(100); // Hide item with index 0 (first element)
-		// If items index + 1 more than list length, then back to to the first item.
-		// In other case subtract to the next item
-		if (curr + 1 >= amount) {
-			curr = 0;
-		} else curr++;
-
-		$(".modalCarouselList li:eq(" + curr + ")").delay(100).fadeIn(100); // Show item with index 0 (first element)
+	modalArrowPrevious.click(function () {
+		owlModalCarousel.trigger('prev.owl.carousel');
+	});
+	modalArrowNext.click(function () {
+		owlModalCarousel.trigger('next.owl.carousel');
 	});
 });
+
 
